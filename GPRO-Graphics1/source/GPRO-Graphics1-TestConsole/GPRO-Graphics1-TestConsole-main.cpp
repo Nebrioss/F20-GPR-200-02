3cb7711510ec0f6a7210fb6f99c66f6e80b6a6bc
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-09-10 14:46:48 -0400

    Chapter 2 content complete

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
@@ -25,9 +25,16 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
+#include <fstream>
+#include <cmath>
 
-
+//#include "color.h"
+//#include "vec3.h"
 #include "gpro/gpro-math/gproVector.h"
+
+
+using std::sqrt;
 
 
 void testVector()
@@ -53,8 +60,38 @@ void testVector()
 }
 
 
+
 int main(int const argc, char const* const argv[])
 {
+	const int image_width = 256;
+	const int image_height = 256;
+
+	std::ofstream outFile("image.ppm");
+
+	outFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
+
+	for (int j = image_height - 1; j >= 0; --j)
+	{
+		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
+		for (int i = 0; i < image_width; ++i)
+		{
+			/*
+			auto r = double(i) / (image_width - 1);
+			auto g = 0;
+			auto b = double(j) / (image_height - 1);
+
+			int ir = static_cast<int>(255.999 * r);
+			int ig = static_cast<int>(255.999 * g);
+			int ib = static_cast<int>(255.999 * b);
+
+			outFile << ir << ' ' << ig << ' ' << ib << '\n';
+			*/
+			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+			write_color(std::cout, pixel_color);
+		}
+	}
+	std::cerr << "\nDone.\n";
+	
 	testVector();
 
 	printf("\n\n");
