775f6035f5926e69f2a2e00315fd481d5c98bc7a
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-09-10 21:21:49 -0400

    Fixed chapter 2 code

--- GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
+++ GPRO-Graphics1/source/GPRO-Graphics1-TestConsole/GPRO-Graphics1-TestConsole-main.cpp
@@ -61,21 +61,22 @@ void testVector()
 
 
 
-int main(int const argc, char const* const argv[])
-{
+int main() {
+
+	// Image
+
 	const int image_width = 256;
 	const int image_height = 256;
 
 	std::ofstream outFile("image.ppm");
 
+	// Render
+
 	outFile << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-	for (int j = image_height - 1; j >= 0; --j)
-	{
+	for (int j = image_height - 1; j >= 0; --j) {
 		std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
-		for (int i = 0; i < image_width; ++i)
-		{
-			/*
+		for (int i = 0; i < image_width; ++i) {
 			auto r = double(i) / (image_width - 1);
 			auto g = 0;
 			auto b = double(j) / (image_height - 1);
@@ -85,11 +86,9 @@ int main(int const argc, char const* const argv[])
 			int ib = static_cast<int>(255.999 * b);
 
 			outFile << ir << ' ' << ig << ' ' << ib << '\n';
-			*/
-			color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
-			write_color(std::cout, pixel_color);
 		}
 	}
+
 	std::cerr << "\nDone.\n";
 	
 	testVector();
