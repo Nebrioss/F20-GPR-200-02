ed15c1930e5fe843c6a8533720e6d4b1de439adb
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-10-26 15:38:07 -0400

    End of Monday's class

--- SHADERed_Files/vertex.glsl
+++ SHADERed_Files/vertex.glsl
@@ -7,14 +7,38 @@
 layout (location = 0) in vec4 aPosition;
 //in vec4 aPosition;
 
+// TRANSFORM UNIFORMS
+uniform mat4 uModelMat;
+uniform mat4 uViewMat;
+uniform mat4 uProjMat;
+uniform mat4 uViewProjMat;
+
 // VARYING
 out vec4 vColor;
 
 void main()
 {
 	// REQUIRED: set this value
-	gl_Position = aPosition;
+	// problem: gl_Position is in "clip-space"
+	// problem: aPosition is in "object-space"
+	//gl_Position = aPosition;
+
+	// position in world-space (wrong)
+	vec4 pos_world = uModelMat * aPosition;
+	//gl_Location = pos_world;
+
+	// position in camera-space (also wrong)
+	//vec4 pos_camera = uViewMat * uModelMat * aPosition;
+	vec4 pos_camera = uViewMat * pos_world;
+	//gl_Position = pos_camera;
+
+	// position in clip-space (finally!)
+	//vec4 pos_clip = uProjMat * uViewMat * uModelMat * aPosition;
+	//vec4 pos_clip = uProjMat * pos_camera;
+	vec4 pos_clip = uViewProjMat * pos_world;
+	gl_Location = pos_clip;
 	
 	//vColor = vec4(1.0, 0.5, 0.0, 1.0);
 	vColor = aPosition;
+	//vColor = pos_camera;
 }
