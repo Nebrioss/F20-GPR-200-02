8016fd1fc95062cece50fbeb6a033183b44ad543
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-10-29 15:41:35 -0400

    Revert "End of Thursday's class"
    This reverts commit aa303afd0daa1e015476d8dbdaf0525b231e0fd5.

--- SHADERed_Files/vertex.glsl
+++ SHADERed_Files/vertex.glsl
@@ -5,7 +5,6 @@
 // e.g. normal
 // e.g. 2D uv: texture coordinate
 layout (location = 0) in vec4 aPosition;
-layout (location = 1) in vec3 aNormal;
 //in vec4 aPosition;
 
 // TRANSFORM UNIFORMS
@@ -15,12 +14,7 @@ uniform mat4 uProjMat;
 uniform mat4 uViewProjMat;
 
 // VARYING
-
-// PER-VERTEX: send final color
-//out vec4 vColor;
-
-// PER-FRAGMENT: send stuff to the FS to calculate final
-out vec4 vNormal;
+out vec4 vColor;
 
 void main()
 {
@@ -30,38 +24,21 @@ void main()
 	//gl_Position = aPosition;
 
 	// position in world-space (wrong)
-	//vec4 pos_world = uModelMat * aPosition;
+	vec4 pos_world = uModelMat * aPosition;
 	//gl_Location = pos_world;
 
 	// position in camera-space (also wrong)
 	//vec4 pos_camera = uViewMat * uModelMat * aPosition;
-	//vec4 pos_camera = uViewMat * pos_world;
+	vec4 pos_camera = uViewMat * pos_world;
 	//gl_Position = pos_camera;
 
 	// position in clip-space (finally!)
 	//vec4 pos_clip = uProjMat * uViewMat * uModelMat * aPosition;
-	//vec4 pos_clip = uViewProjMat * pos_world;
+	//vec4 pos_clip = uProjMat * pos_camera;
+	vec4 pos_clip = uViewProjMat * pos_world;
+	gl_Location = pos_clip;
 	
-	// POSITION PIPELINE
-	mat4 modelViewMat = uViewMat * uModelMat;
-	vec4 pos_camera = modelViewMat * aPosition;
-	vec4 pos_clip = uProjMat * pos_camera;
-	gl_Position = pos_clip;
-	
-	// NORMAL PIPELINE
-	mat3 normalMat = transpose(inverse(mat3(modelViewMat)));
-	vec3 norm_camera = normalMat * aNormal;
-	//vec3 norm_camera = mat3(modelViewMat) * aNormal;
-	
-	// OPTIONAL: set varyings
 	//vColor = vec4(1.0, 0.5, 0.0, 1.0);
-	
-	// DEBUGGING: 
-	
-	// PER-VERTEX: calculate and output final color
-	//vColor = vec4(0.5 * aNormal + 0.5, 1.0);
-	
-	// PER-FRAGMENT: pass things that FS needs to calculate final color
-	//vNormal = vec4(aNormal, 0.0);
-	vNormal = vec4(norm_camera, 0.0);
+	vColor = aPosition;
+	//vColor = pos_camera;
 }
