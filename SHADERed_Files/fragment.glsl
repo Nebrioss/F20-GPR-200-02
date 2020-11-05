66dbce6ad4074ad80291845e4c42caa1ce78fac7
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-11-05 00:12:28 -0500

    Completed basics of light implementation

--- SHADERed_Files/fragment.glsl
+++ SHADERed_Files/fragment.glsl
@@ -7,6 +7,7 @@ precision highp float;
 layout (location = 0) out vec4 rtFragColor;
 //out vec4 rtFragColor;
 
+// TEXTURE UNIFORM
 uniform sampler2D uTexture;
 
 // VARYING
@@ -19,6 +20,43 @@ in vec4 vNormal;
 
 //in vec2 vTexcoord;
 in vec4 vTexcoord;
+
+// LIGHT INFORMATION
+// sLight: struct to keep light elements together
+//	  sCenter: center of light
+//	  sColor: color of light
+//	  sIntensity: intensity of light
+struct sLight
+{
+    vec4 sCenter;
+    vec3 sColor;
+    float sIntensity;
+};
+
+// initLight: initializing light
+//	  light: final light struct
+//	  center: center of light
+//	  color: color of light
+//	  intensity: intensity of light
+void initLight(out sLight light, in vec4 center, in vec3 color, in float intensity)
+{
+    light.sCenter = center;
+    light.sColor = color;
+    light.sIntensity = intensity;
+}
+
+vec3 lambertReflect(sLight light)
+{
+    vec3 lightVector = normalize(light.sCenter.xyz - normalize(vNormal.xyz));
+    vec3 diffuseCoefficient = vNormal.xyz * lightVector;
+    
+    vec3 attenuation = 1.0 / (1.0 + ((light.sCenter.xyz - vec3(vNormal.xyz)) / light.sIntensity) + (((light.sCenter.xyz - vec3(vNormal.xyz)) * (light.sCenter.xyz - vec3(vNormal.xyz)) / (light.sIntensity * light.sIntensity))));
+    
+    vec3 diffuseIntensity = diffuseCoefficient * attenuation;
+
+	return vec3(diffuseIntensity * light.sColor);
+}
+
 
 void main()
 {
@@ -34,6 +72,9 @@ void main()
 	//rtFragColor = vec4(vTexcoord, 0.0, 1.0);
 	rtFragColor = vTexcoord;
 	
-	// USING TEXTURE (hopefully)
-	rtFragColor = texture(uTexture, 2.0 * vTexcoord.xy);
+	// used for testing light
+	rtFragColor = vec4(1.0, 0.5, 0.0, 1.0);
+	
+	// USING TEXTURE
+	//rtFragColor = texture(uTexture, 2.0 * vTexcoord.xy + vec2(0.0, 0.0));
 }
--- SHADERed_Files/vertex.glsl
+++ SHADERed_Files/vertex.glsl
@@ -19,7 +19,7 @@ uniform mat4 uViewMat;
 uniform mat4 uProjMat;
 uniform mat4 uViewProjMat;
 
-// TEXTURE UNIFORM (hopefully works)
+// TEXTURE UNIFORM
 uniform sampler2D uTexture;
 
 // VARYING
@@ -32,6 +32,45 @@ out vec4 vNormal;
 
 //out vec2 vTexcoord;
 out vec4 vTexcoord;
+
+out vec4 vPosition;
+
+// LIGHT INFORMATION
+// sLight: struct to keep light elements together
+//	  sCenter: center of light
+//	  sColor: color of light
+//	  sIntensity: intensity of light
+struct sLight
+{
+    vec4 sCenter;
+    vec3 sColor;
+    float sIntensity;
+};
+
+// initLight: initializing light
+//	  light: final light struct
+//	  center: center of light
+//	  color: color of light
+//	  intensity: intensity of light
+void initLight(out sLight light, in vec4 center, in vec3 color, in float intensity)
+{
+    light.sCenter = center;
+    light.sColor = color;
+    light.sIntensity = intensity;
+}
+
+vec3 lambertReflect(sLight light)
+{
+    vec3 lightVector = normalize(light.sCenter.xyz - vec3(aPosition));
+    vec3 diffuseCoefficient = aNormal * lightVector;
+    
+    vec3 attenuation = 1.0 / (1.0 + ((light.sCenter.xyz - vec3(aPosition)) / light.sIntensity) + (((light.sCenter.xyz - vec3(aPosition)) * (light.sCenter.xyz - vec3(aPosition)) / (light.sIntensity * light.sIntensity))));
+    
+    vec3 diffuseIntensity = diffuseCoefficient * attenuation;
+
+	return vec3(diffuseIntensity * light.sColor);
+}
+
 
 void main()
 {
@@ -87,4 +126,14 @@ void main()
 	vTexcoord = uv_atlas;
 	
 	//gl_Position = uProjMat * modelViewMat * aTexcoord;
+	
+	// LIGHTS
+	sLight lightOne;
+    initLight(lightOne, vec4(1.0, 0.0, 1.0, 0.0), vec3(1.0, 0.75, 0.0), 100.0);
+    
+	sLight lightTwo;
+    initLight(lightTwo, vec4(1.0, 0.0, 1.0, 0.0), vec3(1.0, 0.75, 0.0), 100.0);
+    
+	sLight lightThree;
+    initLight(lightThree, vec4(1.0, 0.0, 1.0, 0.0), vec3(1.0, 0.75, 0.0), 100.0);
 }
